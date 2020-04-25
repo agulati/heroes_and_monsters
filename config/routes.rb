800ed87f6f1d2af56ec416259efc9f3ac2886614
Amit Gulati <agulati@handy.com>
2020-04-25 15:33:01 -0400

    starting quest routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   resources :games, only: [:new, :show, :create], param: :game_id do
     resource :attack, only: [:create]
+    resources :quests, only: [:new, :show, :create, :update]
   end
 
   root to: "games#new"
